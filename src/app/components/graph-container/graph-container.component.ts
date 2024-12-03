ee4ebee8f7898c43d82c5cf220840feb37062414
Vaibhav Gadge <vaibhav.gadge@vanderlande.com>
2024-12-03 17:05:22 +0530

    Updated the code

--- src/app/components/graph-container/graph-container.component.ts
+++ src/app/components/graph-container/graph-container.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-graph-container',
   standalone: true,
-  imports: [MatCardModule, NgxChartsModule],
+  imports: [MatCardModule],
   templateUrl: './graph-container.component.html',
   styleUrl: './graph-container.component.scss'
 })
